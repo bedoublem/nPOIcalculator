92cb947d20b5d93a3ef7ed8de7652150d7192d7b
bedoublem <michaelmoens.be@gmail.com>
2026-03-12 14:11:37 +0100

    Update npoi_configurator.py

--- npoi_configurator.py
+++ npoi_configurator.py
@@ -114,8 +114,8 @@ def construire_ports(nb_secteurs, operateurs, config_freq, tri):
 
     if tri == "Par fréquence":
         ports.sort(key=lambda p: (
-            FREQUENCES_ORDRE.index(p["frequence"]),
-            p["secteur"],
+            FREQUENCES_ORDRE.index(p["secteur"]),
+            p["frequence"],
             p["operateur"],
         ))
     else:
